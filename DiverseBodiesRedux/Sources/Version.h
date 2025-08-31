b019e46998889a6ef7aeb0ed7710a58fcbd41c6c
Panda <kiyanitsa.aleks@gmail.com>
2025-08-31 04:13:41 +0300

    Update project files and version to 2.2.0
    - Removed `BGSCharacterTint.h` and added `BGSChargenUtils.h` in `DiverseBodiesRedux.vcxproj`.
    - Added `BGSCharacterTint.cpp` and `BGSChargenUtils.cpp` to the compilation list.
    - Updated filters in `DiverseBodiesRedux.vcxproj.filters` to reflect the changes.
    - Bumped version number in `Version.h` from "2.1.0" to "2.2.0".
    - Several binary files have been modified.

--- DiverseBodiesRedux/Sources/Version.h
+++ DiverseBodiesRedux/Sources/Version.h
@@ -2,7 +2,7 @@
 
 namespace Version {
 	constexpr auto Name = "DiverseBodies";
-	constexpr auto Version = "2.1.0";
+	constexpr auto Version = "2.2.0";
 	constexpr auto VersionCount = 1;
 	constexpr auto Date = "2025-08-18";
 	constexpr const size_t UID = 'DBR2';
